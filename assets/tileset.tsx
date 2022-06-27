b02a688db84a1c157a978259bd80ae3ad90b0e81
are <aresb@protonmail.com>
2022-06-27 22:50:07 +0200

    Tilset update

--- assets/tileset.tsx
+++ assets/tileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="tileset" tilewidth="32" tileheight="32" tilecount="72" columns="12">
- <image source="../../sprites/platform_metroidvania asset pack v1.01/tiles and background_foreground (new)/tileset_32x32(new).png" width="384" height="192"/>
+<tileset version="1.8" tiledversion="1.8.5" name="tileset_32x32" tilewidth="32" tileheight="32" tilecount="72" columns="12">
+ <image source="img/ground/tileset_32x32.png" width="384" height="192"/>
 </tileset>
