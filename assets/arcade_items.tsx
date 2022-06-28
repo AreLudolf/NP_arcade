73574b339cca968110e3c224a972c8006bd35d90
are <aresb@protonmail.com>
2022-06-28 08:46:56 +0200

    Fix resource in tileset

--- assets/arcade_items.tsx
+++ assets/arcade_items.tsx
@@ -1,67 +1,46 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="arcade_items" tilewidth="24" tileheight="24" tilecount="21" columns="0">
+<tileset version="1.8" tiledversion="1.8.5" name="arcade_items" tilewidth="24" tileheight="24" tilecount="14" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="1">
-  <image width="8" height="10" source="../../sprites/platform_metroidvania asset pack v1.01/miscellaneous sprites/antidote_potion.png"/>
+ <tile id="22">
+  <image width="8" height="10" source="img/items/antidote_potion.png"/>
  </tile>
- <tile id="2">
-  <image width="8" height="8" source="../../sprites/platform_metroidvania asset pack v1.01/miscellaneous sprites/apple_item.png"/>
+ <tile id="23">
+  <image width="8" height="8" source="img/items/apple_item.png"/>
  </tile>
- <tile id="3">
-  <image width="16" height="16" source="../../sprites/platform_metroidvania asset pack v1.01/miscellaneous sprites/arrow_plate_left.png"/>
+ <tile id="24">
+  <image width="16" height="16" source="img/items/arrow_plate_left.png"/>
  </tile>
- <tile id="4">
-  <image width="16" height="16" source="../../sprites/platform_metroidvania asset pack v1.01/miscellaneous sprites/arrow_plate_right.png"/>
+ <tile id="25">
+  <image width="16" height="16" source="img/items/arrow_plate_right.png"/>
  </tile>
- <tile id="5">
-  <image width="16" height="8" source="../../sprites/platform_metroidvania asset pack v1.01/miscellaneous sprites/bigflowers_props.png"/>
+ <tile id="26">
+  <image width="24" height="24" source="img/items/door.png"/>
  </tile>
- <tile id="6">
-  <image width="24" height="24" source="../../sprites/platform_metroidvania asset pack v1.01/miscellaneous sprites/door.png"/>
+ <tile id="27">
+  <image width="16" height="8" source="img/items/flowers_props.png"/>
  </tile>
- <tile id="7">
-  <image width="16" height="8" source="../../sprites/platform_metroidvania asset pack v1.01/miscellaneous sprites/drygrass_props.png"/>
+ <tile id="28">
+  <image width="16" height="8" source="img/items/grass_props.png"/>
  </tile>
- <tile id="8">
-  <image width="16" height="8" source="../../sprites/platform_metroidvania asset pack v1.01/miscellaneous sprites/flowers_props.png"/>
+ <tile id="29">
+  <image width="8" height="10" source="img/items/health_potion.png"/>
  </tile>
- <tile id="9">
-  <image width="16" height="8" source="../../sprites/platform_metroidvania asset pack v1.01/miscellaneous sprites/grass_props.png"/>
+ <tile id="30">
+  <image width="16" height="16" source="img/items/hp.png"/>
  </tile>
- <tile id="10">
-  <image width="8" height="10" source="../../sprites/platform_metroidvania asset pack v1.01/miscellaneous sprites/health_potion.png"/>
+ <tile id="31">
+  <image width="8" height="10" source="img/items/mana.png"/>
  </tile>
- <tile id="11">
-  <image width="16" height="8" source="../../sprites/platform_metroidvania asset pack v1.01/miscellaneous sprites/loot_box_anim.gif"/>
+ <tile id="32">
+  <image width="8" height="8" source="img/items/orb.png"/>
  </tile>
- <tile id="12">
-  <image width="8" height="8" source="../../sprites/platform_metroidvania asset pack v1.01/miscellaneous sprites/orb.png"/>
+ <tile id="33">
+  <image width="16" height="8" source="img/items/root_props.png"/>
  </tile>
- <tile id="13">
-  <image width="16" height="8" source="../../sprites/platform_metroidvania asset pack v1.01/miscellaneous sprites/root_props.png"/>
+ <tile id="34">
+  <image width="16" height="16" source="img/items/stone.png"/>
  </tile>
- <tile id="14">
-  <image width="16" height="16" source="../../sprites/platform_metroidvania asset pack v1.01/miscellaneous sprites/stone.png"/>
- </tile>
- <tile id="15">
-  <image width="8" height="24" source="../../sprites/platform_metroidvania asset pack v1.01/miscellaneous sprites/tiki_torch_props.gif"/>
- </tile>
- <tile id="16">
-  <image width="16" height="16" source="../../sprites/platform_metroidvania asset pack v1.01/miscellaneous sprites/vase.png"/>
- </tile>
- <tile id="17">
-  <image width="16" height="8" source="../../sprites/platform_metroidvania asset pack v1.01/miscellaneous sprites/wood_slab_left.png"/>
- </tile>
- <tile id="18">
-  <image width="16" height="8" source="../../sprites/platform_metroidvania asset pack v1.01/miscellaneous sprites/wood_slab_middle.png"/>
- </tile>
- <tile id="19">
-  <image width="16" height="8" source="../../sprites/platform_metroidvania asset pack v1.01/miscellaneous sprites/wood_slab_right.png"/>
- </tile>
- <tile id="20">
-  <image width="16" height="16" source="img/enemies/enemy_idle.gif"/>
- </tile>
- <tile id="21">
-  <image width="16" height="16" source="img/enemies/enemy_idle.png"/>
+ <tile id="35">
+  <image width="8" height="24" source="img/items/tiki_torch_props.gif"/>
  </tile>
 </tileset>
